bc0e268566a08dacb62def5123078bce16bff861
Juan Francisco <jfmg0509@gmail.com>
2025-02-02 19:30:53 -0500

    CORRECCION DE FALTAS ORTOGRAFICAS EN LOS COMENTARIOS

--- AA2_CREADOR_CONTRASENA.py
+++ AA2_CREADOR_CONTRASENA.py
@@ -2,7 +2,7 @@ numbers= ["1","2","3","4","5","6","7","8","9","0"]
 specials_character=["+","/"]
 while True:
     password_name= input("ingrese el nombre de su contraseña: ")
-    is_number_found= False #esta variable te indica si encontramos números en la cpontraseña
+    is_number_found= False #esta variable te indica si encontramos números en la contraseña
     is_special_character_found= False
     for number in numbers:
         if number in password_name:
@@ -14,7 +14,7 @@ while True:
             break
     if is_number_found  and is_special_character_found:
         print ("tu contraseña es correcta.")
-        break #el breal hace que se cierre el bucle
+        break #el breaK hace que se cierre el bucle
     else:
         print("no te olvides de colocar un número y un caracter especial")
         

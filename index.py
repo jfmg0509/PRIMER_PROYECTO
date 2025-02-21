4be9bebb03d977a58d1071f7fe563cb766d5e129
Juanfran <194420868+jfmg0509@users.noreply.github.com>
2025-02-21 02:41:05 +0000

    EJEMPLO LISTAS

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-numbers = (1,2,3,4)
+"""numbers = (1,2,3,4)
 list_of_numbers = [1,2,3,4]
 dictionary_of_number = {
     "numbers": [1,2,3,4]
@@ -13,4 +13,9 @@ restrictions = {
     "special_character" : [],
 }
 
-print(len(restrictions["numbers"]))  # Esto imprimirá 1
+print(len(restrictions["numbers"]))  # Esto imprimirá 1"""
+
+mi_lista = [2,4,6]
+del mi_lista [1] #elimina el elemento en el índice 1. Recordar que las listas incian con el índice 0
+mi_lista.append (8) # append aumenta el valor 8
+print(mi_lista)

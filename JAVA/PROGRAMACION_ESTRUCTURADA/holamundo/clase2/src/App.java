1a631771bd7c97d7c155f6b64dda3919f07931a9
Juan Francisco <jfmg0509@gmail.com>
2025-05-12 16:06:20 -0500

    CLASE 2

---
+++ JAVA/PROGRAMACION_ESTRUCTURADA/holamundo/clase2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number=5;
+        System.out.println((number++));
+        System.out.println(number);   
+        }
+}
